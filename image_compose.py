3128af392f7cff0fdca6b5f4f516a8ee64e3c56a
Valora <1428424273@qq.com>
2019-09-09 22:02:03 +0800

    图片拼接程序优化

--- image_compose.py
+++ image_compose.py
@@ -2,43 +2,56 @@
 #  作者    : shinevalora
 #  创建时间: 2019/9/8  20:13
 
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)-8s: %(message)s")
+
 
 # 定义图像拼接函数
 def image_compose():
     import os
-    num = len(os.listdir('Image/'))
-
-    print(num)
+    import glob
 
     import cv2
     import numpy as np
 
-    for i in range(1, num + 1, 4):
-        img1 = cv2.imread(f"{i}.jpg")
-        img2 = cv2.imread(f"{i + 1}.jpg")
+    num = len(os.listdir('Image/'))
+    # print(num)
 
-        heng1 = np.hstack((img1, img2))
+    if num % 4 == 0:
 
-        img3 = cv2.imread(f"{i + 2}.jpg")
-        img4 = cv2.imread(f"{i + 3}.jpg")
+        logging.info(f"Image 文件夹下的 .jpg 图片格式的文件共有 {num} 个\n")
 
-        heng2 = np.hstack((img3, img4))
+        for i in range(1, num + 1, 4):
+            logging.info(f"准备读取并合并{glob.glob(f'Image/{i}_*.jpg')[0]}")
+            logging.info(f"准备读取并合并{glob.glob(f'Image/{i + 1}_*.jpg')[0]}")
+            logging.info(f"准备读取并合并{glob.glob(f'Image/{i + 2}_*.jpg')[0]}")
+            logging.info(f"准备读取并合并{glob.glob(f'Image/{i + 3}_*.jpg')[0]}")
 
-        cv2.imwrite(f"hstack_{i}_{i + 1}.jpg", heng1)
-        cv2.imwrite(f"hstack_{i + 2}_{i + 3}.jpg", heng2)
+            img1 = cv2.imread(glob.glob(f'Image/{i}_*.jpg')[0])
+            img2 = cv2.imread(glob.glob(f'Image/{i + 1}_*.jpg')[0])
 
-        img5 = cv2.imread(f"hstack_{i}_{i + 1}.jpg")
-        img6 = cv2.imread(f"hstack_{i + 2}_{i + 3}.jpg")
+            heng1 = np.hstack((img1, img2))
 
-        hebing = np.vstack((img5, img6))
+            img3 = cv2.imread(glob.glob(f'Image/{i + 2}_*.jpg')[0])
+            img4 = cv2.imread(glob.glob(f'Image/{i + 3}_*.jpg')[0])
 
-        cv2.imwrite(f"Image_save/compose_{i}_{i + 1}_{i + 2}_{i + 3}.jpg", hebing)
+            heng2 = np.hstack((img3, img4))
 
-        cv2.waitKey(10)
+            cv2.imwrite(f"Img/hstack_{i}_{i + 1}.jpg", heng1)
+            cv2.imwrite(f"Img/hstack_{i + 2}_{i + 3}.jpg", heng2)
 
-# image_compose()
+            img5 = cv2.imread(f"Img/hstack_{i}_{i + 1}.jpg")
+            img6 = cv2.imread(f"Img/hstack_{i + 2}_{i + 3}.jpg")
+
+            hebing = np.vstack((img5, img6))
+
+            cv2.imwrite(f"Image_save/compose_{i}_{i + 1}_{i + 2}_{i + 3}.jpg", hebing)
+
+            logging.info(f"Image_save/compose_{i}_{i + 1}_{i + 2}_{i + 3}.jpg  拼接并保存成功！\n")
+    else:
+        logging.info(f"Image 文件夹下的 .jpg 图片格式的文件个数必须为4的倍数")
+        exit()
 
 
-
-
-
+image_compose()
