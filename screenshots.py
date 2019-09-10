f31c0b730e78f314ddf98c0e59caabfe0faf69f2
Valora <1428424273@qq.com>
2019-09-10 10:04:10 +0800

    截图程序优化

--- screenshots.py
+++ screenshots.py
@@ -7,7 +7,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)-8s: %(message)s")
 
 
-def screenshots(bbox=(238, 320, 842, 725), num=8):
+def screenshots(bbox=(238, 320, 842, 725)):
     from PIL import ImageGrab, ImageFont, ImageDraw
 
     ttfont = ImageFont.truetype(r"C:\Windows\Fonts\Ebrima.ttf", 26)
@@ -16,9 +16,10 @@ def screenshots(bbox=(238, 320, 842, 725), num=8):
 
     # '截屏区域为：(left, upper, right, lower)-tuple:', bbox)
 
-    num = int(input('请输入你想要截多少张图:'))
+    end_num = int(input('请输入你想要截多少张图:'))
+    start_num=int(input('请输入截图图片的起始数：1-则是从 1_*.jpg   , 2 -则是从 2_*.jpg开始:'))
 
-    for i in range(1, num + 1):
+    for i in range(start_num, start_num+end_num):
         try:
             your_choice = int(input("只能输入 1 2 3 4------{1:'NTC',2:'WT',3:'MU',4:'HET'}: "))
 
